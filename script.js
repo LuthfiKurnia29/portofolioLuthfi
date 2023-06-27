8cc2c8caf3898c709214c02a37ab15e3283aa55f
Luthfi Kurnia Hadi <118035953+LuthfiKurnia29@users.noreply.github.com>
2023-06-27 16:01:22 +0700

    init project
    this is first upload my portofolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// let a = 8;
+// let b = 10;
+// console.log(a + b);
+
+// const navlink = document.querySelectorAll('.nav-link').forEach(e => e.addEventListener('click',function() {
+//     e.style.animation = 'fade-up 3s steps';
+// }))
